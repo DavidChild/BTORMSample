6a2e9c7ce16daefba7bddea0845b8242e47fa94f
黄建斌 <8543569+hjbok@user.noreply.gitee.com>
2021-01-14 15:27:06 +0800

    添加项目文件。

---
+++ BTORMSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BTORMSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
